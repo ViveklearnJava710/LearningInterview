3f1b9a69843643c52d4d21ad851bf48f04ac230c
ViveklearnJava710 <50265664+ViveklearnJava710@users.noreply.github.com>
2019-05-04 20:54:04 +0530

    Add files via upload

--- src/com/vivek/Dinningproblem/Application.java
+++ src/com/vivek/Dinningproblem/Application.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 
 public class Application {
 
-	public static final int NUMBER_OF_Philosopher = 5 ;
+	public static final int NUMBER_OF_PHILOSOPHER = 5 ;
 	public static final int NUMBER_OF_FORK = 5 ;
 
 	public static void main(String[] args) throws InterruptedException {
@@ -16,15 +16,15 @@ public class Application {
 		try {
 
 			System.out.println("Starting ....... ");
-			Philosophers = new Philosopher[NUMBER_OF_Philosopher] ;
+			Philosophers = new Philosopher[NUMBER_OF_PHILOSOPHER] ;
 			Fork[] fork = new Fork[NUMBER_OF_FORK] ;
-			executors = Executors.newFixedThreadPool(NUMBER_OF_Philosopher) ;
+			executors = Executors.newFixedThreadPool(NUMBER_OF_PHILOSOPHER) ;
 
 			for (int i =0 ; i<NUMBER_OF_FORK ; i++ )
 			{
 				fork[i] = new Fork(i) ;
 			}
-			for (int i =0 ; i<NUMBER_OF_Philosopher ; i++ )
+			for (int i =0 ; i<NUMBER_OF_PHILOSOPHER ; i++ )
 			{
 				Philosophers[i] = new Philosopher(i, fork[i], fork[(i +1) % NUMBER_OF_FORK ]) ;
 				executors.execute(Philosophers[i]) ;
